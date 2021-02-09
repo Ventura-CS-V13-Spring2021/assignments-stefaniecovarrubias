4b68f6910ff17f9e2d46160239f95e3958792a74
StefanieCovarrubias <stefaniecovarrubias@gmail.com>
2021-02-09 18:50:49 +0000

    test

---
+++ assignment1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+               const double rateA = 15.00;
+               const double rateB = 12.00;
+               const double rateC = 9.00;
+
+               // Your Code Here.
+
+}
